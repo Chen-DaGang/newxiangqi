14a6e11d4c8246110d065763e0dc312ad298d4fa
CZY and CG <2110469225@qq.com>
2026-04-15 20:47:27 +0800

    ChessGame and some modifications

--- src/main/java/org/example/chess/domain/ChessPieceLocation.java
+++ src/main/java/org/example/chess/domain/ChessPieceLocation.java
@@ -2,7 +2,4 @@ package org.example.chess.domain;
 
 import org.example.chess.domain.entity.Position;
 
-public record ChessPieceLocation(
-        String name,
-        Position position
-) { }
+public record ChessPieceLocation(String name, Position position) { }
